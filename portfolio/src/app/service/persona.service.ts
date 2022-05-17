4fd5e3de09bbdd1e966803f3a2dfeb7aae8ca9f9
bruno bussolotti <brunoxeneize07@gmail.com>
2022-05-17 12:18:06 -0300

    Implementacion en el FrontEnd

--- portfolio/src/app/service/persona.service.ts
+++ portfolio/src/app/service/persona.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
+  URL = 'http://localhost:8080/personas/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL)
+    
+  }
 }
